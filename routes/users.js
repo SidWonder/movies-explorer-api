eb345856a6afb9f010020b5c4c7011de32b037e6
SidWonder <o.mildzikhov@mail.ru>
2021-12-01 01:39:33 +0300

    еще фикс

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.get('/users/me', getUser);
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
   }),
 }), updateProfile);
 
